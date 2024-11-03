6dcd0d40327d5a47e5fc8c6e80a693bbd18427b3
YashDoshi01 <90956721+YashDoshi01@users.noreply.github.com>
2024-11-04 00:18:30 +0530

    all tasks done

--- backend/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const {signup , login} = require("../controllers/AuthController");
-const {signupValidation , LoginValidation} = require("../middlewares/Authvalidation");
-
-router.post('/login' ,LoginValidation , login )
-router.post('/signup' ,signupValidation , signup)
-
-module.exports = router;
